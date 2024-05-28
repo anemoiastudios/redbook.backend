7f1861b7a1863c937d1e3e0e7bcb51b0fb862de1
Joshua Rode <s3845028@student.rmit.edu.au>
2024-05-28 16:52:28 +1000

    testing adding file

---
+++ atlas-searchapi.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import {request} from 'urllib'
+import cors from 'cors'
+import {mongoClient, MONGODB_COLLECTION, MONGODB_DATABASE, User} from './util'
+
+const ATLAS_API_BASE_URL = 'https://cloud.mongodb.com/api/atlas/v1.0'
+const ATLAS_PROJECT_ID = process.env.MONGODB_ATLAS_PROJECT_ID
+const ATLAS_CLUSTER_NAME = process.env.MONGODB_ATLAS_CLUSTER
+const ATLAS_CLUSTER_API_URL = `${ATLAS_API_BASE_URL}/groups/${ATLAS_PROJECT_ID}/clusters/${ATLAS_CLUSTER_NAME}`
+const ATLAS_SEARCH_INDEX_API_URL = `${ATLAS_CLUSTER_API_URL}/fts/indexes`
+
+const ATLAS_API_PUBLIC_KEY = process.env.MONGODB_ATLAS_PUBLIC_KEY
+const ATLAS_API_PRIVATE_KEY = process.env.MONGODB_ATLAS_PRIVATE_KEY
+const DIGEST_AUTH = `${ATLAS_API_PUBLIC_KEY}:${ATLAS_API_PRIVATE_KEY}`
+
+const USER_SEARCH_INDEX_NAME = 'user_search'
+const USER_AUTOCOMPLETE_INDEX_NAME = 'user_autocomplete'
+
+const app = express()
