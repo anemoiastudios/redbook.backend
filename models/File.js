921670e2cd5330e4baf2f80c01040c11644f03e7
brian <s3898120@student.rmit.edu.au>
2024-06-03 13:35:02 +1000

    add fileupload feature

---
+++ models/File.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const fileSchema = new mongoose.Schema({
+    filename: {
+        type: String,
+        required: true
+    },
+    path: {
+        type: String,
+        required: true
+    },
+    channel: {
+        type: String,
+        required: true
+    },
+    uploadedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const File = mongoose.model('File', fileSchema);
+module.exports = File;
