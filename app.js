5cca53bee0e3d8b865943ecc5a85adf6302b2441
Mohnish Deshpande <s3947971@student.rmit.edu.au>
2024-09-20 17:31:11 +1000

    mod: database config

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const profileController = require('./controllers/profileController');
 const channelController = require('./controllers/channelController');
 const { swaggerUi, specs } = require('./swagger');
 const cors = require('cors')
+require('dotenv').config()
+
+// Connect to DB
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB', err);
+    });
 
 const app = express();
 const PORT = 3001;
@@ -12,13 +22,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-mongoose.connect('mongodb://127.0.0.1:27017/anemoia')
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(err => {
-        console.error('Error connecting to MongoDB', err);
-    });
 
 // Swagger UI setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
@@ -27,7 +30,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.get('/api/profile', profileController.getAllProfiles);
 app.get('/api/profile/:username', profileController.getProfileByUsername);
 app.post('/api/createprofile', profileController.createProfile);
-app.post('/api/loginprofile',profileController.loginprofile)
+app.post('/api/loginprofile', profileController.loginprofile)
 app.put('/api/profile/:username', profileController.updateProfileByUsername);
 app.delete('/api/profile/:username', profileController.deleteProfileByUsername);
 
