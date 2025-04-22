62a1cc3d7d08437f117c9e183cb6a2e671bc2a7f
iagonza8 <55616171+iagonza8@users.noreply.github.com>
2025-04-22 11:13:20 -0600

    #518 Uncaught errors on archive.tsx

--- controllers/archivesController.js
+++ controllers/archivesController.js
@@ -1,17 +1,23 @@
 const Archive = require('../models/archive');
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getArchives = async (req, res) => {
-    try {
-        const { userId } = req.params;
-        if (!userId) {
-            return res.status(400).json({ message: "User ID is required" });
-        }
-        const archives = await Archive.find({ userId });
-        res.json(archives);
-    } catch (error) {
-        res.status(500).json({ message: "Error fetching user archives", error });
+  try {
+    const { userId } = req.query;
+    if (!userId) {
+      return res.status(400).json({ message: "User ID is required" });
     }
+    const user = await User.findOne({ username: userId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const archives = await Archive.find({ userId: user._id }); // Use ObjectId
+    res.json(archives);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user archives", error });
+  }
 };
 
 exports.archivePost = async (req, res) => {
